be990bd8bcfba45712ebb9fd1711aa14874a0ec0
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-05-21 11:20:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from train import *
 from optimizers import *
 np.random.seed(1)
 
-def fix(img):
-	return np.array(resize(img, (96,96)).reshape(96,96*3)) / 255.0
+def fix(img, dims):
+	return np.array(resize(img, (dims[0],dims[1])).reshape(dims[0],dims[1]*dims[2])) / 255.0
 
-def grab(path):
+def grab(path, dims):
 	data = []
 	categories = ['hem','all']
 	folders = ['fold_0','fold_1','fold_2']
@@ -22,7 +22,7 @@ def grab(path):
 			p = f'{pt}\\{category}'
 			output = int(category=='hem')
 			for img in os.listdir(p):
-				data.append([fix(imread(os.path.join(p,img))), np.array([output, not(output)])])
+				data.append([fix(imread(os.path.join(p,img)), dims), np.array([output, not(output)])])
 	np.random.shuffle(data)
 	return data
 
@@ -40,16 +40,16 @@ def plot(acc, loss, title):
 def run(data, model, optimizer, path, rate=0.001, beta=0.9, scale=1, epochs=3):
 	start = time()
 	f = open(f'{path}model\\scores.txt', 'r+'); f.truncate(0)
-	l, a, size = [], [], len(data)
+	l, a, dims = [], [], len(data)
 	for epoch in range(epochs):
-		for row in range(size):
+		for row in range(dims):
 			output, loss, accuracy = train(data[row][0], data[row][1], model, optimizer, rate, beta, scale)
 			l.append(loss); a.append(accuracy)
 
 			np.save(f'{path}model\\weights.npy', np.array([model[2].weight, model[2].bias], dtype=object))
 			f = open(f'{path}model\\scores.txt', 'a'); f.write(f'\n{loss}'); f.close()
 
-			print(f'\nITERATION {row+1}/{size} OF EPOCH {epoch+1}/{epochs}:\n')
+			print(f'\nITERATION {row+1}/{dims} OF EPOCH {epoch+1}/{epochs}:\n')
 			print('Output    ➤ ', output)
 			print('Real      ➤ ', data[row][1])
 			print('Loss      ➤ ', '{0:.4f}'.format(loss))
@@ -65,21 +65,23 @@ def run(data, model, optimizer, path, rate=0.001, beta=0.9, scale=1, epochs=3):
 
 print('\nReading Data')
 
-path = input('Model Path: ')
-dataPath = input('Data Path: ')
+path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\CWSF\\KorCNN\\'
+dataPath = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\CWSF\\LeukemiaData\\C-NMC_Leukemia\\training_data'
 
-data = grab(dataPath)
+dims = (96,96,3)
+data = grab(dataPath, dims)
+
 model = [
 	Conv(18,7),
 	Maxpool(4),
-	Softmax(22*70*18, data[0][1].size)
+	Softmax(int((dims[0]/4-2)*(dims[1]*dims[2]/4-2)*18), data[0][1].size) # 22 * 70 * 18
 	]
 
 optimizer = gradient_descent
 rate = 0.005
 beta = 0.9
 scale = 1
-epochs = 1
+epochs = 3
 
 print('\nTraining Model')
 
